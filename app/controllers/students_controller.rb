0fc2a2b8c779b17b8bd65e43f5b84bba4a8f7c58
github username <github email address>
2020-06-07 15:30:09 -0400

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,36 @@
+class StudentsController < ApplicationController
+  def index
+    @students = Student.all
+  end
+
+  def show 
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(post_params)
+    @student.save
+
+    redirect_to student_path(@student)
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.update(post_params)
+
+    redirect_to student_path(@student)
+  end
+
+  private
+
+  def post_params
+    params.require(:student).permit(:first_name, :last_name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :school_classes, only: [:index, :show, :edit, :update, :new, :create]
+  resources :students, only: [:index, :show, :edit, :update, :new, :create]
 end
