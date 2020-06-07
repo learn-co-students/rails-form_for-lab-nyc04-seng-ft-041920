f48ae04d1114401e978edbeece46a41e7598b740
senadakadric <skadric@fordham.edu>
2020-06-07 13:28:44 -0400

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,36 @@
+class SchoolClassesController < ApplicationController
+
+    def index
+        @school_classes = SchoolClass.all
+    end
+
+    def new
+        @school_class = SchoolClass.new
+    end
+
+    def create
+        @school_class = SchoolClass.create(post_params)
+
+        redirect_to school_class_path(@school_class)
+    end
+
+    def show
+        @school_class = SchoolClass.find(params[:id])
+    end
+
+    def edit
+        @school_class = SchoolClass.find(params[:id])
+    end
+
+    def update
+        @school_class = SchoolClass.update(post_params)
+
+        redirect_to school_class_path(@school_class)
+    end
+
+    private
+
+    def post_params
+        params.require(:school_class).permit(:title, :room_number)
+    end
+end
