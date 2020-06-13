d5c4a2006c0b5176ff43990bc0836c9e7183d7a4
labradorescence <orangenewyorkcreativeagency@gmail.com>
2020-06-13 11:31:29 -0400

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,37 @@
+class SchoolClassesController < ApplicationController
+
+      def show
+        @school_class = SchoolClass.find(params[:id])
+      end
+      
+      def new
+        @school_class = SchoolClass.new
+      end
+
+      def create
+        @school_class = SchoolClass.new(school_class_params)
+        @school_class.save #becareful! 
+
+        redirect_to @school_class
+      end
+        
+      def edit
+        @school_class = SchoolClass.find(params[:id])
+      end
+
+      def update
+        @school_class = SchoolClass.find(params[:id])
+        @school_class.update(school_class_params)
+        
+        redirect_to school_class_path
+      end
+    
+    private
+
+    def school_class_params
+
+        params.require(:school_class).permit! #why? 
+        
+    end
+
+end
